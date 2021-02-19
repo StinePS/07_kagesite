84c7be1e14b8230e08ec33067d7c80e75ee7a1f7
Stine Plejdrup <stin039m@stud.kea.dk>
2021-02-19 08:40:54 +0100

    Galleri næsten færdigt inkl. styling og JS - SP

---
+++ script.js
@@ -0,0 +1,70 @@
+//json data-info
+const url = "https://kager-e10d.restdb.io/rest/kager";
+const medieurl = "https://kager-e10d.restdb.io/media/";
+const myHeaders = {
+	headers: {
+		'x-apikey': "602e8f495ad3610fb5bb635c"
+	}
+};
+
+
+//variabler og konstanter
+let cakes;
+
+//hent data, når DOM er loadet
+document.addEventListener("DOMContentLoaded", getData);
+
+async function getData() {
+	console.log("getData");
+	const JSONData = await fetch(url, myHeaders);
+	cakes = await JSONData.json();
+	start();
+}
+
+function start() {
+	console.log("start");
+
+	const filterButtons = document.querySelectorAll("nav button");
+	filterButtons.forEach(button => button.addEventListener("click", filterCakes));
+	showCakes("alle");
+}
+
+
+//eventlistener knyttet til knapperne vælger det aktive filter
+function filterCakes() {
+	console.log("filterCakes");
+	//sæt variablen "filter" til værdien af data-cakes på den klikkede knap
+	const filter = this.dataset.cakes;
+	//fjern class "selected" fra klikket knap
+	document.querySelector(".selected").classList.remove("selected");
+	//tilføj class "selected" til klikket knap
+	this.classList.add("selected")
+	//kald funktion showCakes efter indstilling af nyt filter
+	const h2 = document.querySelector("h2");
+	showCakes(filter);
+	h2.textContent = this.textContent;
+}
+
+
+//funktion der viser retter i liste-view
+function showCakes(filter) {
+	console.log("showCakes");
+	const section = document.querySelector("#gallery");
+	section.textContent = "";
+	cakes.forEach(cake => {
+		if (filter == cake.kategori || filter == "alle") {
+			const cakeTemplate = document.querySelector("template").content;
+			const clone = cakeTemplate.cloneNode(true);
+			clone.querySelector("img").src = medieurl + cake.foto1;
+			clone.querySelector("h3").textContent = cake.kagenavn;
+			clone.querySelector(".short_description").textContent = cake.kort_beskrivelse;
+			clone.querySelector(".cake_time").textContent = `Omtrentligt tidsforbrug: ${cake.tidsforbrug} timer`;
+			// clone.querySelector(".cake_article").addEventListener("click", () => showDetails(cake));
+			section.appendChild(clone);
+		}
+	})
+}
+
+//function showDetails(singleView) {
+//	location.href = `babushka_details.html?id=${singleView._id}`;
+//}
