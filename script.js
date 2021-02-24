7429e80e91fcc908db6ee0e56f311be812210461
Lucas Augustus Frigaard Nyhjem <lucasnyhjem@Lucass-MacBook-Pro.local>
2021-02-24 10:44:57 +0100

    sort function (need help for forEach)

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 const url = "https://kager-e10d.restdb.io/rest/kager";
 const medieurl = "https://kager-e10d.restdb.io/media/";
 const myHeaders = {
-	headers: {
-		'x-apikey': "602e8f495ad3610fb5bb635c"
-	}
+    headers: {
+        'x-apikey': "602e8f495ad3610fb5bb635c"
+    }
 };
 
 //variabler og konstanter
@@ -15,52 +15,79 @@ document.addEventListener("DOMContentLoaded", getData);
 
 //asynkron funktion henter JSON-data og afventer svar tilbage, før "start" køres
 async function getData() {
-	console.log("getData");
-	const JSONData = await fetch(url, myHeaders);
-	cakes = await JSONData.json();
-	start();
+    console.log("getData");
+    const JSONData = await fetch(url, myHeaders);
+    cakes = await JSONData.json();
+    start();
 }
 
 //eventlisteners på filtrerings-knapper + vis alle kager
 function start() {
-	console.log("start");
+    console.log("start");
 
-	const filterButtons = document.querySelectorAll("nav button");
-	filterButtons.forEach(button => button.addEventListener("click", filterCakes));
-	showCakes("alle");
+    const filterButtons = document.querySelectorAll("nav button");
+    filterButtons.forEach(button => button.addEventListener("click", filterCakes));
+    document.querySelector(".sorter_tid").addEventListener("click", sortCakesTime);
+    document.querySelector(".sorter_navn").addEventListener("click", sortCakesName);
+    showCakes("alle");
 }
 
 
 //eventlistener knyttet til knapperne vælger det aktive filter
 function filterCakes() {
-	console.log("filterCakes");
-	//sæt variablen "filter" til værdien af data-cakes på den klikkede knap
-	const filter = this.dataset.cakes;
-	//fjern class "selected" fra klikket knap
-	document.querySelector(".selected").classList.remove("selected");
-	//tilføj class "selected" til klikket knap
-	this.classList.add("selected")
-	//kald funktion showCakes efter indstilling af nyt filter
-	const h2 = document.querySelector("h2");
-	showCakes(filter);
-	h2.textContent = this.textContent;
+    console.log("filterCakes");
+    //sæt variablen "filter" til værdien af data-cakes på den klikkede knap
+    const filter = this.dataset.cakes;
+    //fjern class "selected" fra klikket knap
+    document.querySelector(".selected").classList.remove("selected");
+    //tilføj class "selected" til klikket knap
+    this.classList.add("selected")
+    //kald funktion showCakes efter indstilling af nyt filter
+    const h2 = document.querySelector("h2");
+    showCakes(filter);
+    h2.textContent = this.textContent;
 }
 
+//https://www.javascripttutorial.net/array/javascript-sort-an-array-of-objects/
+function sortCakesTime() {
+    cakes.sort((a, b) => {
+        return a.tidsforbrug - b.tidsforbrug;
+    });
+    //Her skal den gerne starte showCakes med samme filter som sidst, men sorteret
+}
+
+//https://www.javascripttutorial.net/array/javascript-sort-an-array-of-objects/
+function sortCakesName() {
+    cakes.sort((a, b) => {
+        let fa = a.kagenavn.toLowerCase(),
+            fb = b.kagenavn.toLowerCase();
+
+        if (fa < fb) {
+            return -1;
+        }
+        if (fa > fb) {
+            return 1;
+        }
+        return 0;
+    });
+    //Her skal den gerne starte showCakes med samme filter som sidst, men sorteret
+
+}
 
 //funktion der viser kager i liste-view
 function showCakes(filter) {
-	console.log("showCakes");
-	const section = document.querySelector("#gallery");
-	section.textContent = "";
-	cakes.forEach(cake => {
-		if (filter == cake.kategori || filter == "alle") {
-			const cakeTemplate = document.querySelector("template").content;
-			const clone = cakeTemplate.cloneNode(true);
-			clone.querySelector("img").src = medieurl + cake.foto1;
-			clone.querySelector("h3").textContent = cake.kagenavn;
-			clone.querySelector(".cake_time").textContent = `Omtrentligt tidsforbrug: ${cake.tidsforbrug} timer`;
-			clone.querySelector(".cake_article").addEventListener("click", () => location.href = `single_view.html?id=${cake._id}`);
-			section.appendChild(clone);
-		}
-	})
+    console.log("showCakes");
+    const section = document.querySelector("#gallery");
+    section.textContent = "";
+    cakes.forEach(cake => {
+        if (filter == cake.kategori || filter == "alle") {
+            const cakeTemplate = document.querySelector("template").content;
+            const clone = cakeTemplate.cloneNode(true);
+            clone.querySelector("img").src = medieurl + cake.foto1;
+            clone.querySelector("h3").textContent = cake.kagenavn;
+            clone.querySelector(".cake_time").textContent = `Omtrentligt tidsforbrug: ${cake.tidsforbrug} timer`;
+            clone.querySelector(".cake_article").addEventListener("click", () => location.href = `single_view.html?id=${cake._id}`);
+            section.appendChild(clone);
+        }
+    })
 }
